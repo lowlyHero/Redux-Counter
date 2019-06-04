65d528dca13288292ce1895bb7a8b42c1c30c9f8
lowlyHero <e.nunn1009@gmail.com>
2019-06-04 11:37:56 -0400

    Counter fully functional

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,11 +13,11 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
       return {
-        count: state.count++
+        count: state.count + 1
       } 
     case DECREMENT:
       return {
-        count: state.count-- 
+        count: state.count - 1 
       }
     default:
       return state;
